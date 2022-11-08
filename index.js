38c8c4429b3c12db67652698b76a42301db8ce1e
ivan.tsymbal <ivan.t@coreb.in>
2022-11-08 20:00:13 +0200

    test

--- index.js
+++ index.js
@@ -23,14 +23,18 @@ const { PORT = 5000 } = process.env;
 const app = express();
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ['https://test-node-tsimbal.herokuapp.com'],
-    methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
-    credentials: true,
-    origin: true,
-  })
-);
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
