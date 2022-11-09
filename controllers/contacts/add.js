51f3968fa9e05071e8ce6d41b935ffef557e72f1
ivan.tsymbal <ivan.t@coreb.in>
2022-11-09 18:13:45 +0200

    remove contacts

--- controllers/contacts/add.js
+++
@@ -1,9 +0,0 @@
-import Contact from '../../models/Contacts.js';
-
-const getById = async (req, res) => {
-  const result = await Contact.create(req.body);
-
-  res.status(201).json(result);
-};
-
-export default getById;
