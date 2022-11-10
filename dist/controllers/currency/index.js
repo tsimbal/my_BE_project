702fd6eb060427d109dbc4a3e1ebfeb2a5dc22ff
ivan.tsymbal <ivan.t@coreb.in>
2022-11-10 23:19:36 +0200

    update gitignore

--- dist/controllers/currency/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllCurrency = void 0;
-var getAll_1 = require("./getAll");
-Object.defineProperty(exports, "getAllCurrency", { enumerable: true, get: function () { return __importDefault(getAll_1).default; } });
-//# sourceMappingURL=index.js.map
--- dist/middlewares/addHeaders.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const addedHeaders = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Cookie');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    next();
-};
-exports.default = addedHeaders;
-//# sourceMappingURL=addHeaders.js.map
